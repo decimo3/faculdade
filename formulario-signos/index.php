cc06df30b72efc16de69477aae1081fe3d2de5f5
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2022-10-13 13:00:40 -0300

    Mudança no layout do site

--- formulario-signos/index.php
+++ formulario-signos/index.php
@@ -5,7 +5,14 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
-
+	<style type="text/css">
+    .bg-cover {
+      background-attachment: static;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+  </style>
 </head>
 <body>
 <header class="bg-white">
@@ -22,16 +29,15 @@
         $diaInicio = intval(date("z", strtotime("{$mesInicio}/{$diaInicio}"))+1);
         $diaFinal = intval(date("z", strtotime("{$mesFinal}/{$diaFinal}"))+1);
         if (($diaAtual >= $diaInicio) && ($diaAtual <= $diaFinal) || ($constelacao->signoNome == 'Capricornus' && (($diaAtual >= $diaInicio) || ($diaAtual <= $diaFinal)))) {
-          echo '<div class="row border border-dark rounded my-2 p-3 img-responsive">';
-          echo '<div class="col-4">';
-          echo '<img src="./images/' . $constelacao->signoNome . '.jpg" class="img-fluid">'; //
-          echo '</div>';
-          echo '<div class="col-8">';
+          echo '<div class="jumbotron bg-cover" style="background-image: linear-gradient(to bottom, rgba(255,255,255,0.6) 0%,rgba(255,255,255,0.9) 100%), url(./images/' . $constelacao->signoNome . '.jpg)">';
+          // echo '<img src="./images/' . $constelacao->signoNome . '.jpg" class="img-fluid">';
           echo '<p><b>Signo:</b> ' . $constelacao->signoNome . '</p>';
+          echo '<p><b>Período entre: </b>' . $constelacao->dataInicio . ' e ' . $constelacao->dataFim . '</p>';
           echo '<p><b>Descrição:</b> ' . $constelacao->descricao . '</p>';
           echo '</div>';
         }
   		endforeach;
+      }
   	?>
   </section>
   <footer class="border-top bg-white my-2">
