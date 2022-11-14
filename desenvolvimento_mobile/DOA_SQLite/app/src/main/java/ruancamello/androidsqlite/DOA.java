5583587954948725abf3663198880fa55c794859
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2022-11-14 15:25:43 -0300

    DONE: sample methods changed for CRUD database

--- desenvolvimento_mobile/DOA_SQLite/app/src/main/java/ruancamello/androidsqlite/DOA.java
+++ desenvolvimento_mobile/DOA_SQLite/app/src/main/java/ruancamello/androidsqlite/DOA.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DOA {
     public static long inserirCliente(Context context, Cliente cliente) {
         // Criamos uma instância da classe do banco de dados
@@ -21,7 +24,7 @@ public class DOA {
         // O terceiro parâmetro refere-se aos dados que serão adicionados
         return sqLiteDatabase.insert("Cliente", null, values);
     }
-    public static int excluirCliente(Context context, Cliente cliente) {
+    public static int eliminarCliente(Context context, Cliente cliente) {
         // Criamos uma instância da classe do banco de dados
         BancoDeDadosSQLite bancoDeDadosSQLite = new BancoDeDadosSQLite(context);
         // Recuperamos um objeto para escrever no banco de dados
@@ -37,6 +40,17 @@ public class DOA {
         // O segundo parâmetro refere-se a cláusula WHERE
         // O terceiro parâmetro refere-se aos argumentos da cláusula WHERE
         return sqLiteDatabase.delete("Cliente", where, argumentos);
+    }
+    public static int eliminarCliente(Context context) {
+        // Criamos uma instância da classe do banco de dados
+        BancoDeDadosSQLite bancoDeDadosSQLite = new BancoDeDadosSQLite(context);
+        // Recuperamos um objeto para escrever no banco de dados
+        SQLiteDatabase sqLiteDatabase = bancoDeDadosSQLite.getWritableDatabase();
+        // Chamamos pelo método responsável por excluir no banco de dados
+        // O primeiro parâmetro refere-se a tabela
+        // O segundo parâmetro refere-se a cláusula WHERE
+        // O terceiro parâmetro refere-se aos argumentos da cláusula WHERE
+        return sqLiteDatabase.delete("Cliente", null, null);
     }
     public static int atualizarCliente(Context context, Cliente cliente) {
         // Criamos uma instância da classe do banco de dados
@@ -60,11 +74,44 @@ public class DOA {
         // O quarto parâmetro refere-se aos argumentos da cláusula WHERE
         return sqLiteDatabase.update("Cliente", values, where, argumentos);
     }
-    public static Cursor pesquisarTodosClientes(Context context) {
+    public static List<Cliente> pesquisarCliente(Context context) {
         // Criamos uma instância da classe do banco de dados
         BancoDeDadosSQLite bancoDeDadosSQLite = new BancoDeDadosSQLite(context);
         // Recuperamos um objeto para ler do banco de dados
         SQLiteDatabase sqLiteDatabase = bancoDeDadosSQLite.getReadableDatabase();
-        return sqLiteDatabase.query("Cliente", null, null, null, null, null, null);
+        List<Cliente> mClienteList;
+        mClienteList = new ArrayList<>();
+        Cursor cursor = sqLiteDatabase.query("Cliente", null, null, null, null, null, null);
+        cursor.moveToFirst();
+        int i = 0;
+        while (i < cursor.getCount()) {
+            String nome = cursor.getString(1);
+            String phone = cursor.getString(2);
+            Cliente cliente = new Cliente(nome, phone);
+            cliente.setId(cursor.getInt(0));
+            mClienteList.add(cliente);
+            cursor.moveToNext();
+            i++;
+        }
+        cursor.close();
+        return mClienteList;
+    }
+    public static Cliente pesquisarCliente(Context context, String nome) {
+        // Criamos uma instância da classe do banco de dados
+        BancoDeDadosSQLite bancoDeDadosSQLite = new BancoDeDadosSQLite(context);
+        // Recuperamos um objeto para ler do banco de dados
+        SQLiteDatabase sqLiteDatabase = bancoDeDadosSQLite.getReadableDatabase();
+        // Responsável por definir a cláusula WHERE
+        String where = "nome = ?";
+        // Declara um vetor de Strings
+        String[] nomes = {nome};
+        Cursor cursor = sqLiteDatabase.query("Cliente", null, where, nomes, null, null, null);
+        cursor.moveToFirst();
+        String clienteNome = cursor.getString(1);
+        String clienteFone = cursor.getString(2);
+        Cliente cliente = new Cliente(clienteNome, clienteFone);
+        cliente.setId(cursor.getInt(0));
+        cursor.close();
+        return cliente;
     }
 }
